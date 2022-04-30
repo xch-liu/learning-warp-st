afadb0b67365dce3f8566b6edc8c4a348a806434
xch-liu <58788896+xch-liu@users.noreply.github.com>
2022-04-30 11:10:18 +0100

    Update download_model.py

--- texture_rendering/models/download_model.py
+++ texture_rendering/models/download_model.py
@@ -7,7 +7,7 @@ from torch.utils.model_zoo import load_url
 
 # Download the VGG-19 model and fix the layer names
 print("Downloading the VGG-19 model")
-sd = load_url("https://s3-us-west-2.amazonaws.com/jcjohns-models/vgg19-d01eb7cb.pth")
+sd = load_url("https://web.eecs.umich.edu/~justincj/models/vgg19-d01eb7cb.pth")
 map = {'classifier.1.weight':u'classifier.0.weight', 'classifier.1.bias':u'classifier.0.bias', 'classifier.4.weight':u'classifier.3.weight', 'classifier.4.bias':u'classifier.3.bias'}
 sd = OrderedDict([(map[k] if k in map else k,v) for k,v in sd.items()])
 torch.save(sd, path.join("/texture_style_transfer/models", "vgg19-d01eb7cb.pth"))
